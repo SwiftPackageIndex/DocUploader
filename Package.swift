fce88af6c22dcf497be6da7160ef6eb3077df34c
Sven A. Schmidt <sas@finestructure.co>
2022-12-10 09:47:54 +0100

    Re-org files, products, targets, tests

--- Package.swift
+++ Package.swift
@@ -5,18 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "spi-doc-uploader",
+    products: [
+        .executable(name: "doc-uploader", targets: ["Executable"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
-            name: "spi-doc-uploader",
-            dependencies: []),
-        .testTarget(
-            name: "spi-doc-uploaderTests",
-            dependencies: ["spi-doc-uploader"]),
+        .executableTarget(name: "Executable", dependencies: ["DocUploader"]),
+        .target(name: "DocUploader", dependencies: []),
+        .testTarget(name: "DocUploaderTests", dependencies: ["DocUploader"]),
     ]
 )
---
+++ Tests/DocUploaderTests/DocUploaderTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import DocUploader
+
+final class DocUploaderTests: XCTestCase {
+    func testExample() throws {
+    }
+}
