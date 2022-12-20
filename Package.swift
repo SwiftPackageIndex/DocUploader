47347f03d7c01d3333b95060d715506042c01bd4
Sven A. Schmidt <sas@finestructure.co>
2022-12-20 14:13:43 +0100

    Add soto, pin to AWS lambda main for a/a support

--- Package.swift
+++ Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "spi-doc-uploader",
+    platforms: [.macOS(.v12)],
     products: [
         .executable(name: "doc-uploader", targets: ["Executable"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.5.0")),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", branch: "main"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", branch: "main"),
+        .package(url: "https://github.com/soto-project/soto-s3-file-transfer.git", from: "1.1.0"),
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2")
     ],
     targets: [
         .executableTarget(name: "Executable", dependencies: ["DocUploader"]),
         .target(name: "DocUploader", dependencies: [
-            .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
+            .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
             .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+            .product(name: "SotoS3FileTransfer", package: "soto-s3-file-transfer"),
             .product(name: "Zip", package: "Zip")
         ]),
         .testTarget(name: "DocUploaderTests", dependencies: ["DocUploader"]),
