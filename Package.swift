6c376a3cb1bcc58849724feb8446df07cecc0b74
Sven A. Schmidt <sas@finestructure.co>
2023-01-10 12:05:48 +0100

    Update soto-s3-file-transfer (let tasks complete)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", branch: "main"),
         .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", branch: "main"),
-        .package(url: "https://github.com/finestructure/soto-s3-file-transfer.git", revision: "690584351c0c1fd336c8208046f7445e0dd3e9de"),
+        .package(url: "https://github.com/finestructure/soto-s3-file-transfer.git", revision: "a20d5a9ae6a5b3f67b49f01ef6f47c40cc237887"),
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2")
     ],
     targets: [
