01a02cf341c682757a5dea8cc16f8505fe7f9944
Sven A. Schmidt <sas@finestructure.co>
2024-07-02 12:36:30 +0200

    Reapply "Merge pull request #23 from SwiftPackageIndex/switch-zip-library"
    This reverts commit a614c8b595e5191c4835fe42b2e8525f5f579253.

--- Sources/DocUploadBundle/Zipper.swift
+++ Sources/DocUploadBundle/Zipper.swift
@@ -14,16 +14,20 @@
 
 import Foundation
 
-import Zip
+import ZIPFoundation
 
 
 enum Zipper {
     static func zip(paths inputPaths: [URL], to outputPath: URL) throws {
-        try Zip.zipFiles(paths: inputPaths, zipFilePath: outputPath, password: nil, progress: nil)
+        let archive = try Archive(url: outputPath, accessMode: .create)
+        for url in inputPaths {
+            try archive.addEntry(with: url.lastPathComponent, fileURL: url)
+        }
     }
 
     static func unzip(from inputPath: URL, to outputPath: URL, fileOutputHandler: ((_ unzippedFile: URL) -> Void)? = nil) throws {
-        try Zip.unzipFile(inputPath, destination: outputPath, overwrite: true, password: nil, fileOutputHandler: fileOutputHandler)
+        try FileManager.default.createDirectory(at: outputPath, withIntermediateDirectories: true)
+        try FileManager.default.unzipItem(at: inputPath, to: outputPath)
     }
 }
 
